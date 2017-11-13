fc685b28594b1767ca90ff47b73155fe7cb1564c
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-13 10:12:32 -0300

    Se agrega clase Quini6

---
+++ AlgoPoly/src/fiuba/algo3/tp2/algopoly/model/casillero/Quini6.java
@@ -0,0 +1,21 @@
+package fiuba.algo3.tp2.algopoly.model.casillero;
+
+import fiuba.algo3.tp2.algopoly.model.boleta.BoletaQuini6;
+import fiuba.algo3.tp2.algopoly.model.boleta.Premio;
+import fiuba.algos3.tp2.algopoly.Jugador;
+
+public class Quini6 {
+
+    public Quini6() {
+
+    }
+
+    public void actuarSobre(Jugador jugador) {
+        BoletaQuini6 boleto = jugador.getBoletoQuini6();
+        Premio premio = boleto.getPremio();
+
+        jugador.incrementarCapitalEn(premio.getValor());
+
+        boleto.actualizarPremio();
+    }
+}
