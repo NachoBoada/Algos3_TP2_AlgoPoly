49835e84fc781109e901549921ae494bf5bf5a0c
ailinyelen <ailinyelenb@gmail.com>
2017-11-15 16:29:30 -0300

    test modelo avance dinamico

---
+++ test/fiuba/algo3/tp2/algopoly/model/casillero/AvanceDinamico.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.tp2.algopoly.model.casillero;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AvanceDinamico {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
