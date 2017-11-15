095f3d824f0a7a8290db8f4e86eab52ad6ceaa82
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-14 22:45:41 -0300

    Se agregan test integrales 5 y 6

--- test/fiuba/algo3/tp2/algopoly/PrimeraEntregaTest.java
+++ test/fiuba/algo3/tp2/algopoly/PrimeraEntregaTest.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.tp2.algopoly;
 
 import fiuba.algo3.tp2.algopoly.model.Dinero;
+import fiuba.algo3.tp2.algopoly.model.casillero.Carcel;
 import fiuba.algo3.tp2.algopoly.model.casillero.Quini6;
 import fiuba.algos3.tp2.algopoly.Jugador;
 import org.junit.Assert;
@@ -49,9 +50,32 @@ public class PrimeraEntregaTest {
     public void test04JugadorEsPropietarioDeUnBarrioLuegoDeCaerYcomprarlo() {
         Dinero capitalInicial = new Dinero(0);
         Jugador jugador = new Jugador(capitalInicial);
-        Barrio barrio = new Barrio()''
+        Barrio barrio = new Barrio();
         
         jugador.caerEn(barrio);
+        
+        Assert.assertTrue(jugador.esPropietarioDe(barrio));
+    }
+    
+    @Test
+    public void test05JugadorNoPuedeDesplazarseLuegoDeCaerEnCarcel() {
+        Dinero capitalInicial = new Dinero(0);
+        Jugador jugador = new Jugador(capitalInicial);
+        Carcel carcel = new Carcel();
+        
+        jugador.caerEn(carcel);
+        
+        Assert.assertTrue(jugador.esPropietarioDe(barrio));
+    }
+    
+    @Test
+    public void test06JugadorPuedeMoverseLuegoDePagarFianzDeCarcel() {
+        Dinero capitalInicial = new Dinero(0);
+        Jugador jugador = new Jugador(capitalInicial);
+        Carcel carcel = new Carcel();
+        
+        jugador.caerEn(carcel);
+        jugador.pagarFianza();
         
         Assert.assertTrue(jugador.esPropietarioDe(barrio));
     }
