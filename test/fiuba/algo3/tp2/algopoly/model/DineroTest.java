329ffec963ac68d1e259c29f33a88f3f5c832127
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-15 20:46:39 -0300

    Se corrigue DineroTest

--- test/fiuba/algo3/tp2/algopoly/model/DineroTest.java
+++ test/fiuba/algo3/tp2/algopoly/model/DineroTest.java
@@ -8,7 +8,7 @@ public class DineroTest {
     @Test
     public void testEqualsDevuelveTrueAlCompararMismasCantidades() {
         Dinero dinero10 = new Dinero(10);
-        Dinero dinero20 = new Dinero(20);
+        Dinero dinero20 = new Dinero(10);
 
         Assert.assertTrue(dinero10.equals(dinero20));
     }
@@ -19,16 +19,20 @@ public class DineroTest {
         
         dinero10.sumar(new Dinero(10));
 
-        Assert.assertTrue(dinero10.equals(dinero10.getCantidad() + 10));
+        Assert.assertTrue(dinero10.equals(new Dinero(20)));
     }
     
     @Test
     public void testSumaDecrementaEn10LaCantidad() {
-        Dinero dinero10 = new Dinero(10);
+        Dinero dineroInicial = new Dinero(10);
         
-        dinero10.restar(new Dinero(10));
-
-        Assert.assertTrue(dinero10.equals(dinero10.getCantidad() - 10));
+        System.out.println("Antes de restar: " + String.valueOf(dineroInicial.getCantidad()));
+        
+        dineroInicial.restar(new Dinero(10));
+        
+        System.out.println(String.valueOf(dineroInicial.getCantidad()));
+        
+        Assert.assertTrue(dineroInicial.equals(new Dinero(0)));
     }
 
 }
