12cfc8106a69ee8a0d22b93fe603da5b2b23e5de
ailinyelen <ailinyelenb@gmail.com>
2017-11-15 17:13:18 -0300

    casilero.getposicion

--- src/fiuba/algo3/tp2/algopoly/model/casillero/Casillero.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/Casillero.java
@@ -3,6 +3,12 @@ package fiuba.algo3.tp2.algopoly.model.casillero;
 import fiuba.algo3.tp2.algopoly.model.Jugador;
 
 public  abstract class Casillero {
+	
+	private int posicion;
 
     public abstract void actuarSobre(Jugador jugador);
+    
+    public int getPosicion () {
+    	return this.posicion;
+    }
 }
