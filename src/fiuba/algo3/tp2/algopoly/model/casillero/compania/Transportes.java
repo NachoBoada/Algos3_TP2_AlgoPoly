b4e0c908266822f55f3845070eb82b1bd0cf5ab8
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-18 17:14:29 -0300

    Se agrega paquete con modelo compania

---
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Transportes.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.tp2.algopoly.model.casillero.compania;
+
+public class Transportes {
+    
+}
---
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Tren.java
@@ -0,0 +1,12 @@
+package fiuba.algo3.tp2.algopoly.model.casillero.compania;
+
+import fiuba.algo3.tp2.algopoly.model.Jugador;
+
+public class Tren extends Compania {
+
+    @Override
+    public void actuarSobre(Jugador jugador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
