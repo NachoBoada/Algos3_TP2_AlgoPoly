5db072d56fc654ee475633f748262b42dff2695b
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-18 17:17:45 -0300

    Se agrega paquete con modelo compania

--- src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Compania.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Compania.java
@@ -2,6 +2,6 @@ package fiuba.algo3.tp2.algopoly.model.casillero.compania;
 
 import fiuba.algo3.tp2.algopoly.model.casillero.Encasillable;
 
-public abstract class Compania implements Encasillable {
+public abstract class Compania implements Encasillable{
 
 }
