45b018353a54921e5802fd7a9483af97cdad48b3
Nacho <ignacio.boada@outlook.com>
2017-11-19 18:14:14 -0300

    Segunda Entrega Test

---
+++ test/fiuba/algo3/tp2/algopoly/SegundaEntregaTest.java
@@ -0,0 +1,24 @@
+package fiuba.algo3.tp2.algopoly;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fiuba.algo3.tp2.algopoly.model.Dinero;
+import fiuba.algo3.tp2.algopoly.model.Jugador;
+import fiuba.algo3.tp2.algopoly.model.casillero.ImpuestoAlLujo;
+
+public class SegundaEntregaTest {
+
+    @Test
+    public void test15JugadorCaeEnImpuestoAlLujoCon80SaleCon72() {
+    	Dinero dinero = new Dinero(80);
+    	Dinero dineroRestante = new Dinero(72);
+        Jugador jugador = new Jugador(dinero);
+        ImpuestoAlLujo impuesto = new ImpuestoAlLujo();
+        
+        jugador.caerEn(impuesto);
+
+        Assert.assertTrue(jugador.getCapital().equals(dineroRestante));
+    }
+	
+}
