4b71395ca90c246f7a126460bd0d4461a0fd734b
Nacho <ignacio.boada@outlook.com>
2017-11-19 18:15:58 -0300

    cmabio de nombre de test

--- test/fiuba/algo3/tp2/algopoly/SegundaEntregaTest.java
+++ test/fiuba/algo3/tp2/algopoly/SegundaEntregaTest.java
@@ -10,7 +10,7 @@ import fiuba.algo3.tp2.algopoly.model.casillero.ImpuestoAlLujo;
 public class SegundaEntregaTest {
 
     @Test
-    public void test15JugadorCaeEnImpuestoAlLujoCon80SaleCon72() {
+    public void test15JugadorCaeEnImpuestoAlLujoCon80Pierde10PorcientoSaleCon72() {
     	Dinero dinero = new Dinero(80);
     	Dinero dineroRestante = new Dinero(72);
         Jugador jugador = new Jugador(dinero);
