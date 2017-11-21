f60e731bce8bc1c1d97ee33947ec03ca63259da9
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-21 18:57:07 -0300

    Se agrega metodo cobrar en Servicios

--- src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Servicios.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Servicios.java
@@ -1,21 +1,32 @@
 package fiuba.algo3.tp2.algopoly.model.casillero.compania;
 
+import fiuba.algo3.tp2.algopoly.model.Jugador;
+
 public class Servicios {
 
-    private Aysa aysa;
-    private Edesur edesur;
-    
+    private final Aysa aysa;
+    private final Edesur edesur;
+
     public Servicios() {
-    	aysa = new Aysa();
-    	edesur = new Edesur();
+        aysa = new Aysa();
+        edesur = new Edesur();
     }
-    
+
     public Edesur getEdesur() {
         return this.edesur;
     }
-    
+
     public Aysa getAysa() {
         return this.aysa;
     }
-        
+
+    public void cobrar(Compania compania, Jugador jugador) {
+        Jugador duenio = compania.getDuenio();
+        if (duenio.esDuenioDe(compania) && duenio.esDuenioDe(aysa)) {
+            compania.cobrarDoble(jugador);
+        } else {
+            compania.cobrarSimple(jugador);
+        }
+    }
+
 }
