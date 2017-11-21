24738c4f2f3263dabb926be2c7c2f6c1ec90786f
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-21 19:35:46 -0300

    Se agrega logica de cobro en Edesur

--- src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Edesur.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Edesur.java
@@ -5,22 +5,23 @@ import fiuba.algo3.tp2.algopoly.model.Jugador;
 import fiuba.algo3.tp2.algopoly.model.dados.Dados;
 
 public class Edesur extends Compania {
-    
-    private static final Dinero PRECIO = new Dinero(35000);
-    private final int posicion;
-    private Jugador duenio;
 
-    public Edesur() {
+    private static final Dinero PRECIO = new Dinero(35000);
+    private static final int FACTOR_SIMPLE = 500;
+    private static final int FACTOR_DOBLE = 1000;
+
+    private final int posicion;
+    private final Servicios servicios;
+
+    public Edesur(Servicios servicios) {
         super(PRECIO);
         posicion = 3;
+        this.servicios = servicios;
     }
 
     @Override
     public void actuarSobre(Jugador jugador) {
-    	int indiceDeMultiplicidadEdesur = 500;
-    	int ultimaSumaDados = Dados.getInstance().obtenerUltimaSuma();
-    	Dinero dineroADecrementar = new Dinero(ultimaSumaDados * indiceDeMultiplicidadEdesur);
-    	jugador.decrementarCapitalEn(dineroADecrementar);
+        this.servicios.cobrar(this, jugador);
     }
 
     @Override
@@ -28,13 +29,20 @@ public class Edesur extends Compania {
         return this.posicion;
     }
 
+    private void cobrar(Jugador jugador, int factor) {
+        int ultimaSumaDados = Dados.getInstance().obtenerUltimaSuma();
+        Dinero dineroADecrementar = new Dinero(ultimaSumaDados * factor);
+
+        jugador.decrementarCapitalEn(dineroADecrementar);
+    }
+
     @Override
     public void cobrarDoble(Jugador jugador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        cobrar(jugador, FACTOR_DOBLE);
     }
 
     @Override
     public void cobrarSimple(Jugador jugador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        cobrar(jugador, FACTOR_SIMPLE);
     }
 }
