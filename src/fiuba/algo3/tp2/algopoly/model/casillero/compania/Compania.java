518c8bf5ad3757c8ca6e021a6e8cba5ff2dd509e
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-21 18:40:19 -0300

    Se agregan metodos cobrarDoble y cobrarSimple

--- src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Compania.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Compania.java
@@ -4,11 +4,11 @@ import fiuba.algo3.tp2.algopoly.model.Dinero;
 import fiuba.algo3.tp2.algopoly.model.Jugador;
 import fiuba.algo3.tp2.algopoly.model.casillero.Encasillable;
 
-public abstract class Compania implements Encasillable{
+public abstract class Compania implements Encasillable {
 
     protected Jugador duenio;
     protected final Dinero precio;
-    
+
     public Compania(Dinero precio) {
         this.precio = precio;
     }
@@ -17,18 +17,22 @@ public abstract class Compania implements Encasillable{
         return precio;
     }
 
-	public Jugador getDuenio() {
-		return duenio;
-	}
+    public Jugador getDuenio() {
+        return duenio;
+    }
 
-	public void modificarDuenio(Jugador jugador) {
-		this.duenio = jugador;
-	}
-	
-	@Override
-	public abstract void actuarSobre(Jugador jugador);
-	
-	@Override
-	public abstract int getPosicion();
+    public void modificarDuenio(Jugador jugador) {
+        this.duenio = jugador;
+    }
 
+    @Override
+    public abstract void actuarSobre(Jugador jugador);
+
+    @Override
+    public abstract int getPosicion();
+    
+    public abstract void cobrarDoble(Jugador jugador);
+
+    public abstract void cobrarSimple(Jugador jugador);
+    
 }
