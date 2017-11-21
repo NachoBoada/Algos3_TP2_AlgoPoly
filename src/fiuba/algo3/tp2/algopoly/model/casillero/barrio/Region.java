d9cb5d553d39a0e73e56625386a8ffb607f18db8
ailinyelen <ailinyelenb@gmail.com>
2017-11-21 20:51:19 -0300

    se agrega excepcion

--- src/fiuba/algo3/tp2/algopoly/model/casillero/barrio/Region.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/barrio/Region.java
@@ -1,6 +1,8 @@
 package fiuba.algo3.tp2.algopoly.model.casillero.barrio;
 
+import fiuba.algo3.tp2.algopoly.model.CapitalInsuficiente;
 import fiuba.algo3.tp2.algopoly.model.Dinero;
+import fiuba.algo3.tp2.algopoly.model.ElJugadorNoTieneCapitalSuficienteParaEdificar;
 import fiuba.algo3.tp2.algopoly.model.Jugador;
 
 public abstract class Region {
@@ -18,17 +20,23 @@ public abstract class Region {
     
     public void agregarCasa (BarrioDividido barrio, Jugador jugador, Dinero costoCasa) {    
     	if (jugador.esPropietarioDe(barrioSur) && jugador.esPropietarioDe(barrioNorte)) {
+    		try {
+    			jugador.decrementarCapitalEn(costoCasa);
+    		}catch (CapitalInsuficiente e) {throw new ElJugadorNoTieneCapitalSuficienteParaEdificar();}
+    		
     		barrio.sumarCasa();
-    		jugador.decrementarCapitalEn(costoCasa);
     	}
     		
     }
     
-    public void agregarHotel (BarrioDividido barrio, Jugador jugador, Dinero costoCasa) {
+    public void agregarHotel (BarrioDividido barrio, Jugador jugador, Dinero costoHotel) {
     	if ((barrioSur.getCantidadCasas() == 2) && ( barrioNorte.getCantidadCasas() == 2 )) {
     		
+    		try {
+    			jugador.decrementarCapitalEn(costoHotel);
+    		}catch (CapitalInsuficiente e) {throw new ElJugadorNoTieneCapitalSuficienteParaEdificar();}
+
     		barrio.sumarHotel();
-    		jugador.decrementarCapitalEn(costoCasa);
     	}
     }
     
