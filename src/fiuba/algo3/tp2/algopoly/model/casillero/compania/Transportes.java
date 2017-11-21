efe71407369286c458193aa18ba447f08404c70f
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-21 18:43:17 -0300

    Se agrega metodo cobrarBoleto en Transportes

--- src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Transportes.java
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/compania/Transportes.java
@@ -1,13 +1,15 @@
 package fiuba.algo3.tp2.algopoly.model.casillero.compania;
 
+import fiuba.algo3.tp2.algopoly.model.Jugador;
+
 public class Transportes {
 
     private final Subte subte;
     private final Tren tren;
 
     public Transportes() {
-        subte = new Subte();
-        tren = new Tren();
+        subte = new Subte(this);
+        tren = new Tren(this);
     }
 
     public Subte getSubte() {
@@ -16,5 +18,14 @@ public class Transportes {
 
     public Tren getTren() {
         return this.tren;
+    }
+
+    public void cobrarBoleto(Compania compania, Jugador jugador) {
+        Jugador duenio = compania.getDuenio();
+        if (duenio.esDuenioDe(compania) && duenio.esDuenioDe(subte)) {
+            compania.cobrarDoble(jugador);
+        } else {
+            compania.cobrarSimple(jugador);
+        }
     }
 }
