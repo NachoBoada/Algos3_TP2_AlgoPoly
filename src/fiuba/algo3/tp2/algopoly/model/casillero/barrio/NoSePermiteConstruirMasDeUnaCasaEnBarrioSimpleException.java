425f8deab606a38d1c88da997d115e6b52e1a0d5
ailinyelen <ailinyelenb@gmail.com>
2017-11-22 02:08:17 -0300

    se agrega excepcion limite de construccion de casas

---
+++ src/fiuba/algo3/tp2/algopoly/model/casillero/barrio/NoSePermiteConstruirMasDeUnaCasaEnBarrioSimpleException.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.tp2.algopoly.model.casillero.barrio;
+
+import java.lang.RuntimeException;
+
+public class NoSePermiteConstruirMasDeUnaCasaEnBarrioSimpleException extends RuntimeException {
+
+}
