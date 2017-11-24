d0e3218e2916da2ee9131c87eaca01dc6157905f
Nacho <ignacio.boada@outlook.com>
2017-11-24 13:04:19 -0300

    Correccion de variables en Tablero

--- src/fiuba/algo3/tp2/algopoly/model/Tablero.java
+++ src/fiuba/algo3/tp2/algopoly/model/Tablero.java
@@ -10,87 +10,56 @@ import fiuba.algo3.tp2.algopoly.model.casillero.Policia;
 import fiuba.algo3.tp2.algopoly.model.casillero.Quini6;
 import fiuba.algo3.tp2.algopoly.model.casillero.RetrocesoDinamico;
 import fiuba.algo3.tp2.algopoly.model.casillero.Salida;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.BarrioDividido;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.BuenosAires;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.BuenosAiresNorte;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.BuenosAiresSur;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.Cordoba;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.CordobaNorte;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.CordobaSur;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.Neuquen;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.Salta;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.SaltaNorte;
-import fiuba.algo3.tp2.algopoly.model.casillero.barrio.SaltaSur;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.SantaFe;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.Tucuman;
-import fiuba.algo3.tp2.algopoly.model.casillero.compania.Aysa;
-import fiuba.algo3.tp2.algopoly.model.casillero.compania.Edesur;
 import fiuba.algo3.tp2.algopoly.model.casillero.compania.Servicios;
-import fiuba.algo3.tp2.algopoly.model.casillero.compania.Subte;
 import fiuba.algo3.tp2.algopoly.model.casillero.compania.Transportes;
-import fiuba.algo3.tp2.algopoly.model.casillero.compania.Tren;
 
 public class Tablero {
 	
-	private LinkedList<Encasillable> casilleros;
-	
-	/*Creo que no hacen falta los nombres, con poner casilleros.add(new NombreDeLaClase()) alcanza, pero ya lo hice, prefiero comentarlo
-	y borrarlo mas adelante a borarrlo y despues tener que hacerlo*/
-	private final Salida salida;
-	private final Quini6 quini6;
 	private final BuenosAires buenosaires;
-	private final BuenosAiresSur buenosairesSur;
-	private final BuenosAiresNorte buenosairesNorte;
 	private final Servicios servicios;
-	private final Edesur edesur;
-	private final Aysa aysa;
-	private final Carcel carcel;
 	private final Cordoba cordoba;
-	private final CordobaSur cordobaNorte;
-	private final CordobaNorte cordobaSur;
-	private final AvanceDinamico avanceDinamico;
 	private final Transportes transportes;
-	private final Subte subte;
-	private final Tren tren;
-	private final ImpuestoAlLujo impuestoAlLujo;
-	private final SantaFe santafe;
 	private final Salta salta;
-	private final SaltaSur saltaNorte;
-	private final SaltaNorte saltaSur;
-	private final Policia policia;
-	private final Neuquen neuquen;
-	private final RetrocesoDinamico retrocesoDinamico;
-	private final Tucuman tucuman;
-
+	private final Carcel carcel;
 	
+	private LinkedList<Encasillable> casilleros;
+
 	public Tablero(){
+		buenosaires = new BuenosAires();
+		servicios = new Servicios();
+		cordoba = new Cordoba();
+		transportes = new Transportes();
+		salta = new Salta();
+		carcel = new Carcel();
+		
 		casilleros = new LinkedList<Encasillable>();
 		
-		casilleros.add(salida = new Salida());
-		casilleros.add(quini6 = new Quini6());
-		buenosaires = new BuenosAires();
-		casilleros.add(buenosairesSur = buenosaires.getBarrioSur());
-		casilleros.add(buenosairesNorte = buenosaires.getBarrioNorte());
-		servicios = new Servicios();
-		casilleros.add(edesur = servicios.getEdesur());
-		casilleros.add(aysa = servicios.getAysa());
-		casilleros.add(carcel = new Carcel());
-		cordoba = new Cordoba();
-		casilleros.add(cordobaSur = cordoba.getBarrioSur());
-		casilleros.add(cordobaNorte = cordoba.getBarrioNorte());
-		casilleros.add(avanceDinamico = new AvanceDinamico());
-		transportes = new Transportes();
-		casilleros.add(subte = transportes.getSubte());
-		casilleros.add(tren = transportes.getTren());
-		casilleros.add(impuestoAlLujo = new ImpuestoAlLujo());
-		casilleros.add(santafe = new SantaFe());
-		salta = new Salta();
-		casilleros.add(saltaNorte = salta.getBarrioNorte());
-		casilleros.add(saltaSur = salta.getBarrioSur());
-		casilleros.add(policia = new Policia(carcel));
-		casilleros.add(neuquen = new Neuquen());
-		casilleros.add(retrocesoDinamico = new RetrocesoDinamico());
-		casilleros.add(tucuman = new Tucuman());		
+		casilleros.add(new Salida());
+		casilleros.add(new Quini6());
+		casilleros.add(buenosaires.getBarrioSur());
+		casilleros.add(buenosaires.getBarrioNorte());
+		casilleros.add(servicios.getEdesur());
+		casilleros.add(servicios.getAysa());
+		casilleros.add(carcel);
+		casilleros.add(cordoba.getBarrioSur());
+		casilleros.add(cordoba.getBarrioNorte());
+		casilleros.add(new AvanceDinamico());
+		casilleros.add(transportes.getSubte());
+		casilleros.add(transportes.getTren());
+		casilleros.add(new ImpuestoAlLujo());
+		casilleros.add(new SantaFe());
+		casilleros.add(salta.getBarrioNorte());
+		casilleros.add(salta.getBarrioSur());
+		casilleros.add(new Policia(carcel));
+		casilleros.add(new Neuquen());
+		casilleros.add(new RetrocesoDinamico());
+		casilleros.add(new Tucuman());		
 	}
 	
 
