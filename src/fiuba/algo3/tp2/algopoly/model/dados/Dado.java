5c3958e367be0e7062ad232eeaf672a308cfaaff
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-25 17:44:32 -0300

    Se elimina atributo tiro en clase dado, el metodo tirar simplemente devuelve el valor

--- src/fiuba/algo3/tp2/algopoly/model/dados/Dado.java
+++ src/fiuba/algo3/tp2/algopoly/model/dados/Dado.java
@@ -7,14 +7,7 @@ public class Dado {
     private final static int MAX_VALOR = 7;
     private final static int MIN_VALOR = 1;
 
-    private int tiro;
-
-    public Dado() {
-        tiro = 0;
-    }
-    
     public int tirar() {
-        tiro = new Random().nextInt(MAX_VALOR - MIN_VALOR) + MIN_VALOR;
-        return tiro;
+        return new Random().nextInt(MAX_VALOR - MIN_VALOR) + MIN_VALOR;
     }    
 }
