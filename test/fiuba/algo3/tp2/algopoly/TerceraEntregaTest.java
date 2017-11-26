fec1d0ed38a155c710acdb93f08fd9b88aa81513
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-11-26 16:49:03 -0300

    Se agregan test integrales 1 y 2, entrega 3

--- test/fiuba/algo3/tp2/algopoly/TerceraEntregaTest.java
+++ test/fiuba/algo3/tp2/algopoly/TerceraEntregaTest.java
@@ -2,6 +2,7 @@ package fiuba.algo3.tp2.algopoly;
 
 import fiuba.algo3.tp2.algopoly.model.*;
 import fiuba.algo3.tp2.algopoly.model.casillero.barrio.*;
+import fiuba.algo3.tp2.algopoly.model.dados.TiroDeDados;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,12 +11,46 @@ public class TerceraEntregaTest {
     private static final double DELTA = 1e-15;
 
     @Test
-    public void test01Punto3 (){
+    public void test01JugadorTiraDadosAmbosConIgualValorEntoncesPuedeVolverATirar() {
+        /*Un jugador arroja los dados y ambos tienen el mismo valor. Verificar que puede volver a jugar.*/
+        Juego juego = Juego.getInstance();
+        juego.comenzarJuego();
+
+        Jugador primerJugador = juego.getJugadorActual();
+        TiroDeDados tiroDeDados = primerJugador.tirarDadosParaTests(2, 2);
+        juego.turnoProximojugador();
+
+        Assert.assertTrue(tiroDeDados.esDuplicado());
+        Assert.assertEquals(primerJugador, juego.getJugadorActual());
+    }
+
+    @Test
+    public void test02JugadorTiraDadosAmbosConIgualValorEnDosOportunidadesEntoncesSalteaElSegundoTiroYavanzaElTurno() {
+        /*Un jugador arroja los dados y ambos tienen el mismo valor. Verificar que puede volver a jugar.*/
+        Juego juego = Juego.getInstance();
+        juego.comenzarJuego();
+        
+        Jugador primerJugador = juego.getJugadorActual();
+        
+        TiroDeDados primerTiroDeDados = primerJugador.tirarDadosParaTests(2, 2);
+        juego.turnoProximojugador();
+        Assert.assertTrue(primerTiroDeDados.esDuplicado());
+        Assert.assertEquals(primerJugador, juego.getJugadorActual());
+
+        TiroDeDados segundoTiroDeDados = primerJugador.tirarDadosParaTests(2, 2);
+        juego.turnoProximojugador();
+        Assert.assertTrue(segundoTiroDeDados.esDuplicado());
+        Jugador segundoJugador = juego.getJugadorActual();
+        Assert.assertNotEquals(primerJugador, segundoJugador);
+    }
+
+    @Test
+    public void test01Punto3() {
 
         Tablero tablero = new Tablero();
-        Jugador jugador1 = new Jugador(new Dinero(20000),tablero);
-        Jugador jugador2 = new Jugador(new Dinero(100000),tablero);
-        Jugador jugador3 = new Jugador(new Dinero(100000),tablero);
+        Jugador jugador1 = new Jugador(new Dinero(20000), tablero);
+        Jugador jugador2 = new Jugador(new Dinero(100000), tablero);
+        Jugador jugador3 = new Jugador(new Dinero(100000), tablero);
         Region cordoba = new Cordoba();
         Barrio cordobaNorte = new CordobaNorte(cordoba);
         Barrio santaFe = new SantaFe();
@@ -27,7 +62,7 @@ public class TerceraEntregaTest {
 
             jugador1.caerEn(santaFe);
 
-        } catch (ElJugadorDebeVenderPropiedadesPorCapitalInsuficienteException e){
+        } catch (ElJugadorDebeVenderPropiedadesPorCapitalInsuficienteException e) {
 
             jugador1.venderBarrio(cordobaNorte);
 
@@ -36,15 +71,14 @@ public class TerceraEntregaTest {
         jugador2.comprarBarrio(cordobaNorte);
         jugador3.caerEn(cordobaNorte);
 
-        Assert.assertEquals(100000 - 15000 - 20000 + 1300, jugador2.getCapital().getCantidad(),DELTA);
+        Assert.assertEquals(100000 - 15000 - 20000 + 1300, jugador2.getCapital().getCantidad(), DELTA);
     }
 
-    @Test
-    (expected = NoSePuedeConstruirUnHotelEnUnBarrioSimpleException.class)
-    public void testPunto5(){
+    @Test(expected = NoSePuedeConstruirUnHotelEnUnBarrioSimpleException.class)
+    public void testPunto5() {
 
         Tablero tablero = new Tablero();
-        Jugador jugador = new Jugador(new Dinero(100000),tablero);
+        Jugador jugador = new Jugador(new Dinero(100000), tablero);
         Barrio barrioSimple = new SantaFe();
 
         jugador.comprarBarrio(barrioSimple);
