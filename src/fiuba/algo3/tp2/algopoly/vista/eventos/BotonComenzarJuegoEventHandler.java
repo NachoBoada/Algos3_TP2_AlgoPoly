22ec8914311b580e2def1364836b2219728824da
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2017-12-03 12:46:59 -0300

    Se agrega reproduccion de musica en ambas escenas

--- src/fiuba/algo3/tp2/algopoly/vista/eventos/BotonComenzarJuegoEventHandler.java
+++ src/fiuba/algo3/tp2/algopoly/vista/eventos/BotonComenzarJuegoEventHandler.java
@@ -3,6 +3,7 @@ package fiuba.algo3.tp2.algopoly.vista.eventos;
 import fiuba.algo3.tp2.algopoly.model.Juego;
 import fiuba.algo3.tp2.algopoly.vista.ContenedorBienvenidos;
 import fiuba.algo3.tp2.algopoly.vista.ContenedorDados;
+import fiuba.algo3.tp2.algopoly.vista.ContenedorPrincipal;
 import fiuba.algo3.tp2.algopoly.vista.ContenedorProximoJugador;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,18 +13,23 @@ import javafx.stage.Stage;
 
 public class BotonComenzarJuegoEventHandler implements EventHandler<ActionEvent> {
 
+    private final ContenedorPrincipal contenedorPrincipal;
     Stage stage;
     Scene proximaEscena;
-    ContenedorBienvenidos escenaBienvenidos;
+    ContenedorBienvenidos contenedorBienvenidos;
 
-    public BotonComenzarJuegoEventHandler(Stage stage, Scene proximaEscena, ContenedorBienvenidos escenaBienvenidos) {
+    public BotonComenzarJuegoEventHandler(Stage stage, Scene proximaEscena, ContenedorPrincipal contenedorPrincipal, ContenedorBienvenidos contenedorBienvenidos) {
         this.stage = stage;
         this.proximaEscena = proximaEscena;
-        this.escenaBienvenidos= escenaBienvenidos;
+        this.contenedorPrincipal = contenedorPrincipal;
+        this.contenedorBienvenidos= contenedorBienvenidos;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        contenedorBienvenidos.pararMusica();
+        contenedorPrincipal.reproducirMusica();
+
         stage.setScene(proximaEscena);
         stage.setFullScreenExitHint("");
         stage.setFullScreen(true);
